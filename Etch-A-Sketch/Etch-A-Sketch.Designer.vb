c48deb45f61efa85857b372bf5f557e1df15696b
calvin coxen <caponemobster@gmail.com>
2020-10-25 19:11:27 -0600

    Add things, many things

--- Etch-A-Sketch/Etch-A-Sketch.Designer.vb
+++ Etch-A-Sketch/Etch-A-Sketch.Designer.vb
@@ -36,6 +36,8 @@ Partial Class Etchy_Sketchy
         Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.DrawBox = New System.Windows.Forms.PictureBox()
+        Me.ColorShoes = New System.Windows.Forms.ColorDialog()
+        Me.AboutBox = New System.Windows.Forms.ListBox()
         DrawWaveform = New System.Windows.Forms.Button()
         Me.MenuStrip1.SuspendLayout()
         CType(Me.DrawBox, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -43,9 +45,10 @@ Partial Class Etchy_Sketchy
         '
         'DrawWaveform
         '
-        DrawWaveform.Location = New System.Drawing.Point(208, 340)
+        DrawWaveform.Location = New System.Drawing.Point(277, 418)
+        DrawWaveform.Margin = New System.Windows.Forms.Padding(4)
         DrawWaveform.Name = "DrawWaveform"
-        DrawWaveform.Size = New System.Drawing.Size(128, 83)
+        DrawWaveform.Size = New System.Drawing.Size(171, 102)
         DrawWaveform.TabIndex = 1
         DrawWaveform.Text = "Draw Wave form"
         DrawWaveform.UseVisualStyleBackColor = True
@@ -53,37 +56,41 @@ Partial Class Etchy_Sketchy
         '
         'ColorSelect
         '
-        Me.ColorSelect.Location = New System.Drawing.Point(46, 340)
+        Me.ColorSelect.Location = New System.Drawing.Point(61, 418)
+        Me.ColorSelect.Margin = New System.Windows.Forms.Padding(4)
         Me.ColorSelect.Name = "ColorSelect"
-        Me.ColorSelect.Size = New System.Drawing.Size(128, 83)
+        Me.ColorSelect.Size = New System.Drawing.Size(171, 102)
         Me.ColorSelect.TabIndex = 0
         Me.ColorSelect.Text = "Color"
         Me.ColorSelect.UseVisualStyleBackColor = True
         '
         'Clear
         '
-        Me.Clear.Location = New System.Drawing.Point(376, 340)
+        Me.Clear.Location = New System.Drawing.Point(501, 418)
+        Me.Clear.Margin = New System.Windows.Forms.Padding(4)
         Me.Clear.Name = "Clear"
-        Me.Clear.Size = New System.Drawing.Size(128, 83)
+        Me.Clear.Size = New System.Drawing.Size(171, 102)
         Me.Clear.TabIndex = 2
         Me.Clear.Text = "Clear"
         Me.Clear.UseVisualStyleBackColor = True
         '
         'ExitButton
         '
-        Me.ExitButton.Location = New System.Drawing.Point(549, 340)
+        Me.ExitButton.Location = New System.Drawing.Point(732, 418)
+        Me.ExitButton.Margin = New System.Windows.Forms.Padding(4)
         Me.ExitButton.Name = "ExitButton"
-        Me.ExitButton.Size = New System.Drawing.Size(128, 83)
+        Me.ExitButton.Size = New System.Drawing.Size(171, 102)
         Me.ExitButton.TabIndex = 3
         Me.ExitButton.Text = "Exit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
         'MenuStrip1
         '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EdiToolStripMenuItem, Me.HelpToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1067, 28)
         Me.MenuStrip1.TabIndex = 4
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -91,66 +98,81 @@ Partial Class Etchy_Sketchy
         '
         Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
         Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
-        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
         Me.FileToolStripMenuItem.Text = "File"
         '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(93, 22)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(116, 26)
         Me.ExitToolStripMenuItem.Text = "Exit"
         '
         'EdiToolStripMenuItem
         '
         Me.EdiToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectColorToolStripMenuItem, Me.DrawWaveformToolStripMenuItem, Me.ClearToolStripMenuItem})
         Me.EdiToolStripMenuItem.Name = "EdiToolStripMenuItem"
-        Me.EdiToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EdiToolStripMenuItem.Size = New System.Drawing.Size(49, 24)
         Me.EdiToolStripMenuItem.Text = "Edit"
         '
         'SelectColorToolStripMenuItem
         '
         Me.SelectColorToolStripMenuItem.Name = "SelectColorToolStripMenuItem"
-        Me.SelectColorToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.SelectColorToolStripMenuItem.Size = New System.Drawing.Size(199, 26)
         Me.SelectColorToolStripMenuItem.Text = "Select Color"
         '
         'DrawWaveformToolStripMenuItem
         '
         Me.DrawWaveformToolStripMenuItem.Name = "DrawWaveformToolStripMenuItem"
-        Me.DrawWaveformToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.DrawWaveformToolStripMenuItem.Size = New System.Drawing.Size(199, 26)
         Me.DrawWaveformToolStripMenuItem.Text = "Draw Waveform"
         '
         'ClearToolStripMenuItem
         '
         Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
-        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(199, 26)
         Me.ClearToolStripMenuItem.Text = "Clear"
         '
         'HelpToolStripMenuItem
         '
         Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
         Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
-        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(55, 24)
         Me.HelpToolStripMenuItem.Text = "Help"
         '
         'AboutToolStripMenuItem
         '
         Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
-        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
         Me.AboutToolStripMenuItem.Text = "About"
         '
         'DrawBox
         '
-        Me.DrawBox.Location = New System.Drawing.Point(12, 27)
+        Me.DrawBox.Location = New System.Drawing.Point(0, 32)
+        Me.DrawBox.Margin = New System.Windows.Forms.Padding(4)
         Me.DrawBox.Name = "DrawBox"
-        Me.DrawBox.Size = New System.Drawing.Size(776, 307)
+        Me.DrawBox.Size = New System.Drawing.Size(1035, 378)
         Me.DrawBox.TabIndex = 5
         Me.DrawBox.TabStop = False
         '
+        'ColorShoes
+        '
+        Me.ColorShoes.Color = System.Drawing.Color.Red
+        '
+        'AboutBox
+        '
+        Me.AboutBox.FormattingEnabled = True
+        Me.AboutBox.ItemHeight = 16
+        Me.AboutBox.Location = New System.Drawing.Point(0, 32)
+        Me.AboutBox.Name = "AboutBox"
+        Me.AboutBox.Size = New System.Drawing.Size(287, 180)
+        Me.AboutBox.TabIndex = 6
+        '
         'Etchy_Sketchy
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1067, 554)
+        Me.Controls.Add(Me.AboutBox)
         Me.Controls.Add(Me.DrawBox)
         Me.Controls.Add(Me.ExitButton)
         Me.Controls.Add(Me.Clear)
@@ -158,6 +180,7 @@ Partial Class Etchy_Sketchy
         Me.Controls.Add(Me.ColorSelect)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Etchy_Sketchy"
         Me.Text = "Etchy_Sketchy"
         Me.MenuStrip1.ResumeLayout(False)
@@ -181,4 +204,6 @@ Partial Class Etchy_Sketchy
     Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents DrawBox As PictureBox
+    Friend WithEvents ColorShoes As ColorDialog
+    Friend WithEvents AboutBox As ListBox
 End Class
--- Etch-A-Sketch/Etch-A-Sketch.vb
+++ Etch-A-Sketch/Etch-A-Sketch.vb
@@ -1,11 +1,19 @@
-﻿Public Class Etchy_Sketchy
+﻿'Calvin Coxen
+'RCET 0265
+'Fall 2020
+'Etch-a-Sketch
+'https://github.com/CalvinAC/Etch-A-Sketch
+
+Public Class Etchy_Sketchy
 
     Dim drawPen As New System.Drawing.Pen(Color.Black, 1)
     Dim erasePen As New System.Drawing.Pen(Color.FromName("control"), 50)
+    Dim sinePen As New Pen(Color.Black, 6)
+    Dim cosinePen As New Pen(Color.Black, 6)
+    Dim tangentPen As New Pen(Color.Black, 6)
     Dim g As Graphics = Me.CreateGraphics
     Dim lastX As Integer
     Dim lastY As Integer
-
 
     Sub Draw(x As Integer, y As Integer)
         g = DrawBox.CreateGraphics
@@ -22,8 +30,12 @@
     End Sub
 
     Sub ColorChooser()
-        ' ColorChoose.ShowDialog()
-        'pen.Color=ColorChoose.Color
+        ColorShoes.ShowDialog()
+        drawPen.Color = ColorShoes.Color
+        sinePen.Color = ColorShoes.Color
+        cosinePen.Color = ColorShoes.Color
+        tangentPen.Color = ColorShoes.Color
+
     End Sub
 
     Sub Eraser(x As Integer, y As Integer)
@@ -41,28 +53,65 @@
         End If
     End Sub
 
+    Private Sub DrawBox_MouseMove(sender As Object, e As MouseEventArgs) Handles DrawBox.MouseUp
+        lastX = 0
+        lastY = 0
+    End Sub
+
 
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
 
     Private Sub DrawWaveform_Click(sender As Object, e As EventArgs)
+        AboutBox.Visible = False
         Dim x As Double
         Dim y As Double
-        Dim graphPen As New Pen(Color.Black, 6)
 
         g = DrawBox.CreateGraphics
 
-        For r As Double = 0 To 900
-            y = Math.Sin(r / 280 * 2 * Math.PI) * 100 + 100
-            x = r
+        For n As Double = 0 To 1000
+            y = Math.Sin(n / 280 * 2 * Math.PI) * 100 + 150
+            x = n
 
-            g.DrawLine(graphPen, CType(x, Single), CType(y, Single), CType(x, Single) + 1, CType(y, Single))
+            g.DrawLine(sinePen, CType(x, Single), CType(y, Single), CType(x, Single) + 1, CType(y, Single))
+
+        Next
+
+        For n As Double = 0 To 1000
+            y = Math.Cos(n / 280 * 2 * Math.PI) * 100 + 150
+            x = n
+
+            g.DrawLine(cosinePen, CType(x, Single), CType(y, Single), CType(x, Single) + 1, CType(y, Single))
+
+        Next
+
+        For n As Double = 0 To 1000
+            y = Math.Tan(n / 550 * 2 * Math.PI) * 20 + 150
+            x = n
+
+            g.DrawLine(tangentPen, CType(x, Single), CType(y, Single), CType(x, Single) + 1, CType(y, Single))
 
         Next
     End Sub
 
     Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        AboutBox.Visible = False
         DrawBox.Image = Nothing
+    End Sub
+
+    Private Sub ColorSelect_Click(sender As Object, e As EventArgs) Handles ColorSelect.Click
+        AboutBox.Visible = False
+        ColorChooser()
+    End Sub
+
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        AboutBox.Visible = True
+        AboutBox.Items.Add(" Press the button to do the thing")
+    End Sub
+
+    Private Sub Etchy_Sketchy_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AboutBox.Visible = False
     End Sub
 End Class
